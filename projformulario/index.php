ab03820c2984467ae207ba81dcabf24ba9336768
Cristiano La Féria <138156003+crislaferia@users.noreply.github.com>
2023-12-29 10:45:13 +0000

    agrupamentomaterial

---
+++ projformulario/index.php
@@ -0,0 +1,20 @@
+<!-- index.html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seu Index HTML</title>
+</head>
+<body>
+    <header>
+        <div id="cabecalho">
+            <?php include "php/cabecalho.php"; ?>
+            
+        </div>
+        <div id="menu">
+            <?php include "php/menu.php"; ?>
+        </div>
+    </header>
+</body>
+</html>
---
+++ projformulario/php/cabecalho.php
@@ -0,0 +1,10 @@
+
+    <div style="background-color:#2c2e3e; height: 120px">
+    
+    <img src="https://cencal.pt/versions/v7.1.2.0/public/branding.501886354/logo.png?a=1698141962 " width=120 height=100> 
+    
+    <a class="corner-topbar" href="#" onclick="sair()">Sair</a>
+       
+    
+    <a class="corner-topbar"  href="#" onclick="osmeusformularios()">Os meus formulários</a>
+</div>
