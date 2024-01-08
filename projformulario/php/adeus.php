744f0c4dade5d481c75859738c570b7edf81122e
Cristiano La Féria <138156003+crislaferia@users.noreply.github.com>
2024-01-08 16:32:58 +0000

    adicionei adeus.php

---
+++ projformulario/php/adeus.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="../css/style.css" />
+    <title>Sessão Terminada</title>
+
+</head>
+<body>
+    <header class="container">
+        <img
+        loading="lazy"
+        srcset="https://cencal.pt/versions/v7.1.2.0/public/branding.501886354/logo.png?a=1698141962"
+        class="logo"
+      />
+    </header>
+    <section class="contentend">
+        <div class="section">
+            <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/f284db328070a13755c0bec0a003968fb88ed31bc5064ca306a3bab76fb40bb8?apiKey=f707913968e64dada259d321cef35518&" class="section-img" />
+            <header class="title">Sessão Terminada com sucesso!</header>
+        </div>
+    </section>
+</body>
+</html>
--- projformulario/php/cabecalho.php
+++ projformulario/php/cabecalho.php
@@ -3,7 +3,7 @@
     
     <img src="https://cencal.pt/versions/v7.1.2.0/public/branding.501886354/logo.png?a=1698141962 " width=120 height=100> 
     
-    <a class="corner-topbar" href="#" onclick="sair()">Sair</a>
+    <a class="corner-topbar" href="/adeus.php">Sair</a>
        
     
     <a class="corner-topbar"  href="#" onclick="osmeusformularios()">Os meus formulários</a>
