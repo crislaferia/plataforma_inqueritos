a09d4c2597bc63dd5612fbf9538cc077fc95aa24
Felipe Pires de Moraes <felipemoraes_amb@hotmail.com>
2024-01-15 17:15:38 +0000

    versaoenvialink

---
+++ projformulario/php/phpligaemail.php
@@ -0,0 +1,61 @@
+<?php
+
+include 'PHPMailer.php';
+include 'Exception.php';
+include 'SMTP.php';
+include 'testeenviolink.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Função para enviar e-mails
+function enviarEmails($listaEmails) {
+    // Criar uma instância do PHPMailer
+    $mail = new PHPMailer (true);
+
+    // Configurar as informações do servidor de e-mail
+    $mail->isSMTP();
+    $mail->Host = 'mail.cencal.pt';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'platinq@cencal.pt';
+    $mail->Password = 'C6aJEFdI(=1k';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+    // Configurar o remetente
+    $mail->setFrom('platinq@cencal.pt', 'Plataforma de Inquéritos');
+
+    // Loop para enviar e-mails para cada endereço
+    $emails = explode(',', $listaEmails);
+    foreach ($emails as $email) {
+        $mail->addAddress(trim($email));
+
+        // Configuração do corpo do e-mail, assunto, etc.
+        $mail->Subject = 'Assunto do E-mail';
+        $mail->Body = "Bem vindo ao Cencal, clique no link abaixo para aceder o formulário:";
+
+        // Enviar o e-mail
+        if ($mail->send()) {
+            echo 'E-mail enviado com sucesso para ' . $email . '<br>';
+        } else {
+            echo 'Erro ao enviar o e-mail para ' . $email . ': ' . $mail->ErrorInfo . '<br>';
+        }
+
+        // Limpar os destinatários para o próximo e-mail
+        $mail->clearAddresses();
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $emailInputado = $_POST['email'];
+
+    // Exemplo de chamada da função com o email inputado pelo usuário
+    enviarEmails($emailInputado);
+}
+
+// Exemplo de chamada da função
+//$listaEmails = 'felipemoraes_amb@hotmail.com';
+//enviarEmails($listaEmails);
+
+?>
---
+++ projformulario/php/testeenviolink.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de E-mail</title>
+</head>
+<body>
+    <form action="phpligaemail.php" method="post">
+        <label for="email">Email:</label>
+        <input type="email" name="email" required>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
